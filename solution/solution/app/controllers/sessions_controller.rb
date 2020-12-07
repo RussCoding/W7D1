e637bd8305b7667a7d4a29ab57505f4d6c03ae03
russelkamal <russelhkamal@gmail.com>
2020-12-07 17:27:16 -0500

    done

--- solution/solution/app/controllers/sessions_controller.rb
+++ solution/solution/app/controllers/sessions_controller.rb
@@ -6,11 +6,19 @@ class Session < ApplicationController
    def create
         user = User.find_by_credentials(params[:user][:username],params[:user][:password])
         if user
-            session[:session_token] = user.reset_session_token
+            session[:session_token] = user.reset_session_token!
             redirect_to cats_url
         end
    end
 
    def destroy
-   end
+     if  !!@current_user
+          @current_user.reset_session_token!
+          session[:session_token] = nil
+          redirect_to new_session_url
+     else
+          puts "Not logged in"
+          nil
+     end
+     
 end
--- solution/solution/app/controllers/users_controller.rb
+++ solution/solution/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class User < ApplicationController
     def create
         @user = User.new(user_params)
         if user.save
-            session[:session_token] = @user.reset_session_token
+            session[:session_token] = @user.reset_session_token!
             redirect_to cats_url
         else
             render :new
